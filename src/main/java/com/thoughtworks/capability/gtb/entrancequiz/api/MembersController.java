bca8f857affd849966537ba186b734b8aaa91069
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-07 21:51:00 +0800

    [lk]add function of add group member

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/MembersController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/MembersController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.capability.gtb.entrancequiz.api;
 
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Members;
 import com.thoughtworks.capability.gtb.entrancequiz.service.MembersService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,21 @@ import java.util.List;
 
 @RestController
 public class MembersController {
-    private MembersService membersService;
+    private final MembersService membersService;
 
-    @GetMapping("/members")
-    public List<Members> getAllMembers(){
-        return membersService.getAllMembers();
+    public MembersController(MembersService membersService) {
+        this.membersService = membersService;
     }
+    @GetMapping("/members")
+    @CrossOrigin
+    public List<Members> getAllMembers() {
+        List<Members> list = membersService.getAllMembers();
+        return list;
+    }
+
     @PostMapping("/add-members")
-    public void addMembers(String name){
+    @CrossOrigin
+    public void addMembers(String name) {
         membersService.addMembers(name);
     }
 }
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/MembersService.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/service/MembersService.java
@@ -1,16 +1,21 @@
 package com.thoughtworks.capability.gtb.entrancequiz.service;
 
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Members;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Service
 public class MembersService {
     private List<Members> allMembers = initAllMembers();
+    private int index = 1;
 
     public List<Members> initAllMembers() {
+        Members member = new Members();
+        member.Members(1,"廖浚斌");
         List<Members> members = new ArrayList<>();
+        members.add(member);
         return members;
     }
 
@@ -19,8 +24,10 @@ public class MembersService {
     }
 
     public void addMembers(String name){
+        index++;
         Members member = new Members();
-        member.Members(2,name);
+        member.Members(index,name);
+        allMembers.add(member);
     }
 
 }
