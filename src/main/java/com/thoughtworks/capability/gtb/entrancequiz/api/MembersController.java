2040eb64222b37dadbbfa9b15f600d2f5bd3c713
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-08 03:31:29 +0800

    [lk] add getMapping of getStudentGroup

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/MembersController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/MembersController.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.capability.gtb.entrancequiz.api;
 
+import com.thoughtworks.capability.gtb.entrancequiz.domain.Group;
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Members;
 import com.thoughtworks.capability.gtb.entrancequiz.service.MembersService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,5 +28,25 @@ public class MembersController {
     public ResponseEntity addMembers(@PathVariable("name") String name) {
         membersService.addMembers(name);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/membersGroup")
+    @CrossOrigin
+    public ResponseEntity<List<Group>> getStudentsGroup() {
+        return ResponseEntity.ok(membersService.getStudentsGroup());
+    }
+
+    @PutMapping("/team/{originName}/{name}")
+    @CrossOrigin
+    public ResponseEntity changeTeamName(@PathVariable("originName") String originName,
+                                         @PathVariable("name") String name) {
+        try{
+            membersService.changeTeamName(originName, name);
+            return ResponseEntity.ok().build();
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+
     }
 }
